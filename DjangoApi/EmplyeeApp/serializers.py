fc260131d83f69b7bfea86f347993893b27df4bf
olupy <fortuneosho@gmail.com>
2022-05-13 23:57:44 +0100

    saved

--- DjangoApi/EmplyeeApp/serializers.py
+++ DjangoApi/EmplyeeApp/serializers.py
@@ -4,5 +4,18 @@ from EmplyeeApp.models import Departments, Employees
 class DepartmentSerializers(serializers.ModelSerializer):
     class Meta:
         model = Departments
-        fields = ('')
+        fields = ('DepartmentId',
+                  'DepartmentName',
+        )
+
+class EmployeeSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Employees
+        fields = (
+            'EmployeeId',
+            'EmployeeName',
+            'Department',
+            'DateofJoining',
+            
+        )
 
